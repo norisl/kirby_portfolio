510d4c77ff10424fd652376f4527d0c60f7273f5
Nora Gailer <nora.gailer@zhdk.ch>
2016-10-04 19:39:10 +0200

    udpadet: @media query handler > js for responsive media && back arrow click function (not working)

--- kirby/assets/js/main.js
+++ kirby/assets/js/main.js
@@ -1,9 +1,26 @@
 $(document).pjax('a[data-pjax]', '#pjax-container');
 $(document).pjax(".teaser-project-linkaser");
 
-
 $(document).ready(function () {
-	var colors = ["#FF358B", "#01B0F0", "#AEEE00", "#000000"];
+
+var handleMediaChange = function (mediaQueryList) {
+    if (mediaQueryList.matches) {
+		$('.teaser-project-link').click( function(event) {
+	 	$('.project-detail').css({'display':'block', 'float':'left', 'width':'95%', 'margin': '0 1rem 0 1rem'});
+	 	$('.project-overview').css( {'position':'fixed', 'left':' -100%', 'display':'none'});
+	 	$('.arrow').css({'display':'block'});
+		})
+	}
+};
+
+
+var mql = window.matchMedia("screen and (max-width: 45rem)");
+	mql.addListener(handleMediaChange);
+	handleMediaChange(mql);
+
+
+
+var colors = ["#FF358B", "#01B0F0", "#AEEE00", "#000000"];
 	var index = 0;
 	
 	$ (this).click(function (){
@@ -14,15 +31,13 @@ $(document).ready(function () {
 		
 	});
 
-	if (window.matchMedia('(max-width: 35rem)').matches){
-		$( '.teaser-project-link').click( function(event) {
-			console.log('fiired');
-	 	$('.project-detail').css({'display':'block', 'float':'left', 'width':'95%', 'margin': '0 1rem 0 1rem'});
-	 	$('.project-overview').css( {'position':'fixed', 'left':' -100%', 'display':'none'});
-		});
-	};
-
 	// var path = document.querySelector('.path');
 	// var length = path.getTotalLength();
 
  });
+
+
+$('.arrow').click(function(){
+		$('.project-overview').css( {'position':'fixed', 'left':' 0%', 'display':'block'});
+		$('.project-detail').css({'display':'none'});
+	});
